ed5b346877c4ef46af0ac84e5342065240193633
Olov <olov.92@gmail.com>
2020-04-23 11:18:01 +0200

    Added all files from travel-site project to this repo.

---
+++ cloud-functions/secret-area.js
@@ -0,0 +1,24 @@
+exports.handler = function(event, context, callback) {
+const secretContent = `
+<h3> Welcome to the <strong>secret</strong> area</h3>
+`
+
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+}
